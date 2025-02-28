bcdbdbed7a5e539a0451bb2e260e233f02c50006
rohansapkal7 <rohansapkal.void@gmail.com>
2025-02-28 23:04:37 +0530

    [update] Project_card.jsx

--- src/components/grid_items/Project_card.jsx
+++ src/components/grid_items/Project_card.jsx
@@ -3,7 +3,23 @@ import React from 'react'
 function Project_card({data}) {
   return (
     <div className='project-card-box'>
-        <p></p>
+        <div className="project-header">
+          <p className="project-title">{data.project_title}</p>
+          <div className="projects-icons">
+
+          </div>
+        </div>
+
+        <div className="project-content">
+          <p className="project-subt">
+
+          </p>
+          <p className="project-desc">
+
+          </p>
+        </div>
+
+        <p className="project-techs"></p>
     </div>
   )
 }
